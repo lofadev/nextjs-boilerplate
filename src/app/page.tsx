b9acfe4a5d258dc911e8676dd76dfd2e2b00904c
lofa <contact.lofa@gmail.com>
2025-03-14 19:02:38 +0700

    feat: components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+import { Loader2 } from 'lucide-react';
+
 const Page = () => {
   return (
     <div>
       <h1>NextJS 15 + Shadcn UI + TailwindCss 4 + Typescript</h1>
+      <Button>
+        <Loader2 className='animate-spin' /> Submit
+      </Button>
     </div>
   );
 };
